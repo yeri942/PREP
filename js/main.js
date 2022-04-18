80f75b45bed5ae0a46e7044174a27ad5f9f7657c
김예리 <dpfl0445@gmail.com>
2022-04-18 23:53:50 +0900

    connect 업그레이드 day4(마우스 위치에 따라 그림자 크기 변함 | 마우스가 떠날때 그림자 축소)

--- js/main.js
+++ js/main.js
@@ -28,13 +28,26 @@ clickLogo = () => {
         document.getElementById("goToNextPage").style.display = "none";
         sns[0].addEventListener("mousemove", (e) => {
             const clientRect = sns[0].getBoundingClientRect();
-            let verticalCenter = clientRect.top + clientRect.height / 3;
-            let horizonCenter = clientRect.left + clientRect.width / 3;
-            console.log(e.clientX, e.clientY);
-            console.log("요소 좌표 ", clientRect.top - clientRect.height / 2);
+            let verticalCenter = clientRect.top + clientRect.height / 2;
+            let horizonCenter = clientRect.left + clientRect.width / 2;
+            console.log(horizonCenter - e.clientX, verticalCenter - e.clientY);
+            let shadowSize =
+                (Math.abs(horizonCenter - e.clientX) +
+                    Math.abs(verticalCenter - e.clientY)) /
+                2;
+            sns[0].style.transitionDuration = "0s";
             sns[0].style.boxShadow = `${horizonCenter - e.clientX}px ${
                 verticalCenter - e.clientY
-            }px 0px 10px #012e6485`;
+            }px 0px ${shadowSize}px #012e6485`;
+        });
+        sns[0].addEventListener("mouseout", (e) => {
+            sns[0].style.transitionDuration = "0.5s";
+            sns[0].style.boxShadow = "0px 0px 0px #012e6485";
         });
     }, 3400);
+};
+
+const clickConnect = (e) => {
+    e.style.transitionDuration = "0.5s";
+    e.style.boxShadow = "0px 0px 0px 100px #012e6485";
 };
