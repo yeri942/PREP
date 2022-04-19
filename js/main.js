cd90e028e9c00ae9f9b868936521127713c33de7
김예리 <dpfl0445@gmail.com>
2022-04-19 23:59:26 +0900

    connect 업그레이드 day5(클릭시 그림자 사라짐 & 영역 차오르는 효과)

--- js/main.js
+++ js/main.js
@@ -5,6 +5,32 @@ const merch = document.getElementById("merch");
 const connect = document.getElementById("connect");
 const footer = document.getElementsByTagName("footer");
 const sns = document.getElementsByClassName("sns");
+const bingle = document.getElementsByClassName("bingle");
+
+const snsList = [
+    {
+        name: "facebook",
+        color: "#012e6485",
+        link: "https://www.facebook.com/prepband",
+    },
+    {
+        name: "instagram",
+        color: "#0000009f",
+        link: "https://www.instagram.com/prepband",
+    },
+    {
+        name: "youtube",
+        color: "#9b1914",
+        link: "https://www.youtube.com/channel/UCphD5BdaBNVJxbH8XZKLDqg",
+    },
+    {
+        name: "twitter",
+        color: "#0a658a",
+        link: "https://twitter.com/prep_band",
+    },
+];
+
+let snsN = 1;
 
 clickLogo = () => {
     logo.style.transform = "rotate(0deg)";
@@ -26,8 +52,8 @@ clickLogo = () => {
     }, 2100);
     setTimeout(() => {
         document.getElementById("goToNextPage").style.display = "none";
-        sns[0].addEventListener("mousemove", (e) => {
-            const clientRect = sns[0].getBoundingClientRect();
+        sns[snsN].addEventListener("mousemove", (e) => {
+            const clientRect = sns[snsN].getBoundingClientRect();
             let verticalCenter = clientRect.top + clientRect.height / 2;
             let horizonCenter = clientRect.left + clientRect.width / 2;
             console.log(horizonCenter - e.clientX, verticalCenter - e.clientY);
@@ -35,19 +61,28 @@ clickLogo = () => {
                 (Math.abs(horizonCenter - e.clientX) +
                     Math.abs(verticalCenter - e.clientY)) /
                 2;
-            sns[0].style.transitionDuration = "0s";
-            sns[0].style.boxShadow = `${horizonCenter - e.clientX}px ${
+            sns[snsN].style.transitionDuration = "0s";
+            sns[snsN].style.boxShadow = `${horizonCenter - e.clientX}px ${
                 verticalCenter - e.clientY
             }px 0px ${shadowSize}px #012e6485`;
         });
-        sns[0].addEventListener("mouseout", (e) => {
-            sns[0].style.transitionDuration = "0.5s";
-            sns[0].style.boxShadow = "0px 0px 0px #012e6485";
+        sns[snsN].addEventListener("mouseout", () => {
+            deleteShadow(0);
+        });
+        sns[snsN].addEventListener("click", () => {
+            deleteShadow(0);
+            setTimeout(() => {
+                bingle[snsN].style.animation = "spin 2s infinite";
+            }, 500);
+            setTimeout(() => {
+                window.open("https://www.facebook.com/prepband");
+                bingle[snsN].style.animation = "none";
+            }, 2500);
         });
     }, 3400);
 };
 
-const clickConnect = (e) => {
-    e.style.transitionDuration = "0.5s";
-    e.style.boxShadow = "0px 0px 0px 100px #012e6485";
+const deleteShadow = () => {
+    sns[snsN].style.transitionDuration = "0.5s";
+    sns[snsN].style.boxShadow = "0px 0px 0px #012e6485";
 };
